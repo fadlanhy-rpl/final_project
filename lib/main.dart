999c93e9c87d4bf6d3920b47d4faa038fa6ce21c
Fadlan Hidayat <mfh.fadlanhidayat@gmail.com>
2025-11-27 09:40:38 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:final_project/screens/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'SkyNews App',
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData.light(),      
+      // darkTheme: ThemeData.dark(),
+
+      home: const SplashScreen(),
+    );
+  }
+}
